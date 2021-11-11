b9643fd222d733a6c88d6bc15edb65e55639e128
Bruno Grecco <88808730+greccomelo@users.noreply.github.com>
2021-11-10 22:51:42 -0300

    Versões finais SA2

--- script/script.js
+++ script/script.js
@@ -1,8 +1,24 @@
 // Jquery
 
-$( document ).ready( function() {
+// carrossel
+$( document ).ready(function () {
+    $("#carrossel img:eq(0)").addClass("banner-ativo").show()
 
-    $("#barras").click(function() {
+setInterval(slide, 3000)
+
+function slide() {
+    if ( $(".banner-ativo").next().length ) {
+         $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+    } else {
+         $(".banner-ativo").removeClass().hide()
+         $("#carrossel img:eq(0)").addClass("banner-ativo").show()
+
+    }
+}
+
+// menu barras
+$( document ).ready(function () {
+    $("#barras").click(function () {
         $("#menu").toggleClass("menu-ativo")
 
         // if($("#menu").hasClass("menu-ativo")){
@@ -11,50 +27,34 @@ $( document ).ready( function() {
         //     $("#menu").addClass("menu-ativo")
         // }
     })
-
+})
 })
 
-// sub-menu categorias
-$( document ).ready( function() {
 
-    $("#menu_categoria").click(function() {
+// sub-menu categorias
+$( document ).ready(function () {
+    $("#menu_categoria").click(function () {
         $("#sub_categoria").toggleClass("menu_sub_categoria")
     })
-
 })
 
 // sub-menu jogos
-$( document ).ready( function() {
-
-    $("#jogo").click(function() {
+$( document ).ready(function () {
+    $("#jogo").click(function () {
         $("#jogos").toggleClass("menu_jogos")
     })
-
 })
 
 
-let recebe = document.getElementById("entrada");
+$( document ).ready(function () {    
+    let menu = document.getElementById("menu");
 
-function pesquisa() {
-    let digitado = recebe.value;
-
-    console.log(digitado)
-}
-
-let menu=document.getElementById("menu");
-
-function mostrarMenu() {
-    if (menu.style.display == "none") {
-        menu.style.display = "flex"       
-    } else{
-        menu.style.display = "none"
+    function mostrarMenu() {
+        if (menu.style.display == "none") {
+            menu.style.display = "flex"
+        } else {
+            menu.style.display = "none"
+        }
     }
-}
+})
 
-let recebe1 = document.getElementById("entrada768");
-
-function pesquisa1() {
-    let digitado1 = recebe1.value;
-
-    console.log(digitado1)
-}
